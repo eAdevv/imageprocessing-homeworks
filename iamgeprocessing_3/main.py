e56785466556e2a847b3e9a2f8ff31d97784b236
Emin <arslanemin26@gmail.com>
2022-10-31 21:36:08 +0300

    HW

---
+++ iamgeprocessing_3/main.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('image.jpg',0)
+
+hist,bins = np.histogram(img.flatten(),256,[0,256])
+
+cdf = hist.cumsum()
+cdf_normalized = cdf * hist.max()/ cdf.max()
+cv2.imshow('img',img)
+plt.plot(cdf_normalized, color = 'b')
+plt.hist(img.flatten(),256,[0,256], color = 'r')
+plt.xlim([0,256])
+plt.legend(('cdf','histogram'), loc = 'upper left')
+plt.show()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
